e9d10e07f6e4cca7595296c7c1712e3c36ce8a87
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2013-10-30 11:37:17 -0400

    models

--- nepi/main/models.py
+++ nepi/main/models.py
@@ -2,19 +2,45 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+class ICAP(models.Model):
+    '''How do we differentiate between ICAP admins
+    (those who add modules/content) and those who 
+    simply edit schools etc.'''
+    region = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
 
-class Institution(models.Model):
+    def __unicode__(self):
+        return self.user.username + " " + self.user.region
+
+class School(models.Model):
 	'''Some of the countries have fairly long names,
 	assuming the schools may also have long names.'''
 	country = models.CharField(max_length=100)
 	name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.user.name + " " + self.user.country
+
+class Teacher(models.Model):
+    '''Assuming each school has many teachers but each
+    teacher works at only one school.'''
+    user = models.ForeignKey(User, related_name="teacher")
+    school = models.ForeignKey(School)
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.user.name + " " + self.user.school
+
+class LearningModule(models.Model):
+    '''Need to store learning modules.'''
+    content = models.CharField(max_length=200)
 
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, related_name="application_user")
     last_location = models.CharField(max_length=255)
     visited = models.TextField()
-    institution = models.ForeignKey(Institution)
+    school = models.ForeignKey(School)
 
     def __unicode__(self):
         return self.user.username
@@ -58,8 +84,6 @@ post_save.connect(create_user_profile, sender=User)
 
 
 
-class LearningModule(models.Model):
-	content = models.CharField(max_length=200)
 
 
 
