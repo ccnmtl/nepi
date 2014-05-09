eebeb2b52246f002cae18d0e942c5f13e1151de2
Anders Pearson <anders@columbia.edu>
2014-05-09 17:38:30 -0400

    flake8ion

--- nepi/activities/test/test_models.py
+++ nepi/activities/test/test_models.py
@@ -1,16 +1,17 @@
 from factories import UserFactory, ConversationScenarioFactory
-from factories import ConversationResponseFactory, ConvClickFactory
+from factories import ConversationResponseFactory
 from django.contrib.auth.models import User
 from django.test import TestCase
 from nepi.activities.models import ConversationScenario, \
     Conversation, ConversationResponse, ConvClick
-from datetime import datetime
+
 
 class TestConversationScenario(TestCase):
     '''We want to make sure we can create a conversation
      response associated with the user upon submission.'''
     def test_submission(self):
-        user = User.objects.create_user('person', 'email@emailperson.com', 'personpassword')
+        user = User.objects.create_user(
+            'person', 'email@emailperson.com', 'personpassword')
         user.save()
         scenario = ConversationScenario.objects.create()
         scenario.save()
@@ -18,36 +19,16 @@ class TestConversationScenario(TestCase):
         conversation.save()
         click = ConvClick.objects.create(conversation=conversation)
         click.save()
-        response = ConversationResponse.objects.create(conv_scen=scenario, user=user, first_click=click)
+        response = ConversationResponse.objects.create(
+            conv_scen=scenario, user=user, first_click=click)
         response.save()
-        
-        
-        
-
-    
-#     def test_submission(self):
-#         cs = ConversationScenarioFactory()
-#         # possibilities for submissions
-#         # it is first user submission - no
-#         # conversation response has been created yet
-
-        #submit
-        # user has clicked on one conversation and clicks on same one
-        # user has clicked on both conversations
-        # user
-        # self.assertEqual(str(cs), "Conversation Scenario")
 
     def test_first_submission(self):
         # on first user submission - no
         # conversation response has been created yet
-        cs = ConversationScenarioFactory()
-        user = UserFactory()
-        cr = ConversationResponseFactory()
-        #cs.submit(user, cr)
-        #cr = ConversationResponseFactory()
-        #cr.scenario = cs
-        #cr.user = user
-        #cr.save()
+        ConversationScenarioFactory()
+        UserFactory()
+        ConversationResponseFactory()
 
 
 class TestConversation(TestCase):
