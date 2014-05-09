0ac440ee71fd02863f7f6b7469ef4ba045f72e66
Cassandra Dunlop <cdunlop@columbia.edu>
2014-05-09 17:52:25 -0400

    still updating tag

--- nepi/main/templatetags/conversation_state.py
+++ nepi/main/templatetags/conversation_state.py
@@ -18,52 +18,23 @@ class ConversationState(template.Node):
             if response.first_click:
                 return response.first_click.id
         except ConversationResponseDoesNotExist:
-            return 0
+            return '0'
 
 @register.tag('get_response')
 def get_response(parser, token):
     user = token.split_contents()[1:][0]
     senario = token.split_contents()[1:][1]
-    response = token.split_contents()[1:][2]
-    return ConversationState(user, senario, response)
+    try:
+        response = token.split_contents()[1:][2]
+    except IndexError:
+        response = '0'
+        #is it okay to return different varaibles?
+        #can you return something other than a ConversationState object?
+    return ConversationState(user, senario)
 
-# @register.tag('ifaccessible')
-# def accessible(parser, token):
-#     section = token.split_contents()[1:][0]
-#     nodelist_true = parser.parse(('else', 'endifaccessible'))
-#     token = parser.next_token()
-#     if token.contents == 'else':
-#         nodelist_false = parser.parse(('endifaccessible',))
-#         parser.delete_first_token()
-#     else:
-#         nodelist_false = None
-#     return AccessibleNode(section, nodelist_true, nodelist_false)
+
 # @register.tag('get_user_session_state')
 # def get_user_session_state(parser, token):
 #     user = token.split_contents()[1:][0]
 #     session_id = token.split_contents()[1:][1]
 #     return GetUserSessionState(user, session_id)
-# 
-# 
-# class CoversationNode(template.Node):
-#     def __init__(self, section, nodelist_true, nodelist_false=None):
-#         self.nodelist_true = nodelist_true
-#         self.nodelist_false = nodelist_false
-#         self.section = section
-# 
-# 
-#     def render(self, context):
-#         s = context[self.section]
-#         
-#         if 'request' in context:
-#             r = context['request']
-#             u = r.user
-#     
-#             visited, last_section = s.gate_check(u)
-# 
-#             if s.get_previous().submitted(u):# and visited:
-#                 return self.nodelist_true.render(context)
-# 
-#         return self.nodelist_false.render(context)
-# 
-# 
