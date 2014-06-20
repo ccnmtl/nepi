9a33165c5590e0464dc3dac27ec16611e0afad17
Cassandra Dunlop <cdunlop@columbia.edu>
2014-06-20 15:50:11 -0400

    local changes

--- nepi/main/admin.py
+++ nepi/main/admin.py
@@ -4,7 +4,7 @@ from pagetree.models import Hierarchy
 from django.contrib.auth.models import User
 
 from nepi.main.models import UserProfile, \
-    Course, School, Country, PendingTeachers
+    Group, School, Country, PendingTeachers
 
 
 class UserProfileInline(admin.StackedInline):
@@ -29,7 +29,7 @@ admin.site.register(Hierarchy)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(Course)
+admin.site.register(Group)
 admin.site.register(School)
 admin.site.register(Country)
 admin.site.register(PendingTeachers)
