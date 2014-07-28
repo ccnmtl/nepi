dd2852ecb44579091406f736c58cf13881299b9f
Cassandra Dunlop <cdulop@columbia.com>
2014-07-28 11:44:13 -0400

    minor tweaks to registration flow

--- nepi/urls.py
+++ nepi/urls.py
@@ -45,6 +45,8 @@ urlpatterns = patterns(
      TemplateView.as_view(template_name="flatpages/registration_thanks.html")),
     (r'^thank_you_school/',
      TemplateView.as_view(template_name="flatpages/school_added.html")),
+    (r'^account_created/',
+     TemplateView.as_view(template_name="flatpages/account_created.html"))
 )
 
 
