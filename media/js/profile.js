a05974e51f0a0c1ed9402da505bfc7721cb1a238
Susan Dreher <sdreher@columbia.edu>
2014-09-23 09:34:38 -0400

    fix for pmt#96879

--- media/js/profile.js
+++ media/js/profile.js
@@ -25,7 +25,7 @@ jQuery(document).ready(function () {
                 disableFacultyAccess();
             },
             success: function (json, textStatus, xhr) {
-                if (json['schools'].length < 2) {
+                if (json['schools'].length < 1) {
                     // There are no schools for this country
                     disableFacultyAccess();
                 } else {
