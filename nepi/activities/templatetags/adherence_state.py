7b726c07b568d106a44119c0a01b5af4fb01142d
Susan Dreher <sdreher@columbia.edu>
2014-12-10 14:59:55 -0500

    filter Submissions rather than using .get. Adding tests as well.

--- nepi/activities/templatetags/adherence_state.py
+++ nepi/activities/templatetags/adherence_state.py
@@ -12,26 +12,27 @@ class GetUserResponse(template.Node):
         self.var_name = var_name
 
     def render(self, context):
-        try:
-            ctype = ContentType.objects.get_for_model(Quiz)
-            blocks = PageBlock.objects.filter(
-                content_type__pk=ctype.pk,
-                css_extra__contains=self.quiz_identifier)
-            ids = blocks.values_list('object_id', flat=True)
+        ctype = ContentType.objects.get_for_model(Quiz)
+        blocks = PageBlock.objects.filter(
+            content_type__pk=ctype.pk,
+            css_extra__contains=self.quiz_identifier)
+        ids = blocks.values_list('object_id', flat=True)
 
-            quizzes = Quiz.objects.filter(id__in=ids)
-            quiz = quizzes[0]  # there should just be one
-            user = context['request'].user
-            user_submission = Submission.objects.get(user=user, quiz=quiz)
-            response = Response.objects.get(submission=user_submission)
-            question = Question.objects.get(id=response.question.id)
-            answer = question.correct_answer_values()[0]
+        quizzes = Quiz.objects.filter(id__in=ids)
+        quiz = quizzes[0]  # there should just be one
+        user = context['request'].user
+
+        # multiple quick submits may result in multiple submissions
+        # this protects against that case.
+        submissions = Submission.objects.filter(user=user, quiz=quiz)
+        if len(submissions) > 0:
+            user_submission = submissions[0]
+            responses = Response.objects.filter(submission=user_submission)
+            if len(responses) > 0:
+                response = responses[0]
+                question = Question.objects.get(id=response.question.id)
+                answer = question.correct_answer_values()[0]
             context[self.var_name] = answer
-            return ''
-        except Submission.DoesNotExist:
-            return ''
-        except IndexError:
-            return ''
         return ''
 
 
--- nepi/activities/tests/test_templatetags.py
+++ nepi/activities/tests/test_templatetags.py
@@ -1,7 +1,12 @@
 from django.test.client import RequestFactory
 from django.test.testcases import TestCase
+from pagetree.models import Hierarchy
+from pagetree.tests.factories import ModuleFactory
+from quizblock.models import Quiz, Question, Answer, Submission, Response
+
 from nepi.activities.models import DosageActivity, DosageActivityResponse, \
     ConversationScenario, ConversationResponse
+from nepi.activities.templatetags.adherence_state import GetUserResponse
 from nepi.activities.templatetags.conversation_state import \
     ConversationStateNode
 from nepi.activities.templatetags.dosage_state import DosageStateNode
@@ -56,3 +61,59 @@ class TestConversationStateNode(TestCase):
         context = dict(request=self.request, block=self.block)
         node.render(context)
         self.assertEquals(context['the_response'], 0)
+
+
+class TestAdherenceStateNode(TestCase):
+    def setUp(self):
+        ModuleFactory("one", "/pages/one/")
+        self.hierarchy = Hierarchy.objects.get(name='one')
+        self.section = self.hierarchy.get_root().get_first_leaf()
+
+        # Setup a quiz block
+        self.quiz = Quiz.objects.create()
+        self.question = Question.objects.create(
+            quiz=self.quiz, text="single", question_type="single choice")
+        Answer.objects.create(question=self.question, label="a", value="a",
+                              correct=True)
+        Answer.objects.create(question=self.question, label="b", value="b")
+        self.section.append_pageblock("Quiz One", "adherence", self.quiz)
+
+    def set_quiz_response(self, user, value):
+        # complete + right quiz
+        submission = Submission.objects.create(quiz=self.quiz, user=user)
+        Response.objects.create(question=self.question,
+                                submission=submission, value=value)
+
+    def test_get_response_no_state(self):
+        request = RequestFactory().get('/pages/%s/' % self.hierarchy.name)
+        request.user = UserFactory()
+
+        node = GetUserResponse('adherence', 'the_response')
+        context = dict(request=request)
+        node.render(context)
+        self.assertFalse('the_response' in context)
+
+    def test_get_response_one_submission(self):
+        user = UserFactory()
+        self.set_quiz_response(user, 'a')
+
+        request = RequestFactory().get('/pages/%s/' % self.hierarchy.name)
+        request.user = user
+
+        node = GetUserResponse('adherence', 'the_response')
+        context = dict(request=request)
+        node.render(context)
+        self.assertEquals(context['the_response'], 'a')
+
+    def test_get_response_multiple_submissions(self):
+        user = UserFactory()
+        self.set_quiz_response(user, 'b')
+        self.set_quiz_response(user, 'b')
+
+        request = RequestFactory().get('/pages/%s/' % self.hierarchy.name)
+        request.user = user
+
+        node = GetUserResponse('adherence', 'the_response')
+        context = dict(request=request)
+        node.render(context)
+        self.assertEquals(context['the_response'], 'a')
